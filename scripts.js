bfb1d222bdd7468a3a7a687608a3c5f7aa6bed79
ArooshaAsif <arooshaasif3185@gmail.com>
2024-07-09 10:27:04 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Form submission
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Message sent!');
+});
